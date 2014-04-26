fe4553cad1d4d90c3514ce6ad0aca7a7a465f2a9
george--v <g3ovalle@gmail.com>
2014-04-26 17:50:36 +0100

    Checking commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## This is for Programming Assignment2
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
